9068cf55bc94d640688fb0d45a3ff9348acb6da5
Ilyas Ezzahid <ezzil1997@gmail.com>
2019-07-30 21:24:03 +0700

    customizing the Admin Area

--- listings/admin.py
+++ listings/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
-
 from .models import Listings
 
+
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ( 'id', 'title', 'is_published','price', 'city', 'realtor')
+    list_display_links = ('id', 'title')
+    list_filter = ('realtor', )
+    list_editable = ('is_published', )
+    list_per_page = 25
+    search_fields = ('title',)
+
+
+
 # Register your models here.
-admin.site.register(Listings)
+admin.site.register(Listings, ListingAdmin)
