4a11da68866bdb25f5699a741422522a29791b85
Ilyas Ezzahid <ezzil1997@gmail.com>
2019-08-05 17:19:02 +0700

    contact autofield

--- contacts/admin.py
+++ contacts/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Contact
 
 # Register your models here.
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'listing', 'email', 'phone', 'contact_date', )
+    list_display_links = ('id', 'name')
+    list_per_page = 25
+    search_fields = ('id', 'name', 'listing', 'email') 
+
+
+# register the changes
+admin.site.register(Contact, ContactAdmin)
--- contacts/views.py
+++ contacts/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def contact(request):
+    if request.method == 'POST':
+        return print('message sended!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
